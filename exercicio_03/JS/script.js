be4fedc6bbf3917cf14b2aeb13ddf96eb2430a8d
Thaina Leticia Rossinoli <103609562+rossinoli@users.noreply.github.com>
2024-09-28 22:54:13 -0300

    exercicio codigo limpo

--- exercicio_03/JS/script.js
+++ exercicio_03/JS/script.js
@@ -1,23 +1,22 @@
+// Calcula o Máximo Divisor Comum (MDC) de dois números usando o algoritmo de Euclides.
 function calcularMDC(num1, num2) {
-    // Algoritmo de Euclides
-    while (num2 !== 0) {
-      const temp = num2;
-      num2 = num1 % num2;
-      num1 = temp;
-    }
-    return num1;
+  while (num2 !== 0) {
+    const temp = num2;
+    num2 = num1 % num2;
+    num1 = temp;
   }
+  return num1;
+}
 
-  function obterNumerosEcalcular() {
-    const num1 = parseInt(prompt("Digite o primeiro número:"));
-    const num2 = parseInt(prompt("Digite o segundo número:"));
+// Obtém dois números do usuário e exibe o MDC.
+function obterNumerosEcalcular() {
+  const num1 = parseInt(prompt("Digite o primeiro número:"));
+  const num2 = parseInt(prompt("Digite o segundo número:"));
 
-    const mdc = calcularMDC(num1, num2);
+  const mdc = calcularMDC(num1, num2);
 
-    // Exibir o resultado em um elemento HTML
-    const resultadoElemento = document.getElementById('resultado');
-    resultadoElemento.textContent = `O MDC de ${num1} e ${num2} é: ${mdc}`;
-  }
+  document.getElementById('resultado').textContent = `O MDC de ${num1} e ${num2} é: ${mdc}`;
+}
 
-  // Chamar a função para iniciar o processo
-  obterNumerosEcalcular();
+// Inicia o processo de cálculo do MDC.
+obterNumerosEcalcular();
