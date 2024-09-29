763388c2ec452223f5def639a13da132b755b7ad
Thaina Leticia Rossinoli <103609562+rossinoli@users.noreply.github.com>
2024-09-28 22:47:28 -0300

    exercicio codigo limpo

--- exercicio_02/JS/script.js
+++ exercicio_02/JS/script.js
@@ -1,25 +1,28 @@
+// Array de objetos contendo informações de alunos, como nome, nota1 e nota2.
 const alunos = [
-    { nome: 'Gabriel', nota1: 3, nota2: 6 },
-    { nome: 'Thaina', nota1: 9, nota2: 10 },
-    { nome: 'Suelen', nota1: 6, nota2: 8 }
+  { nome: 'Gabriel', nota1: 3, nota2: 6 },
+  { nome: 'Thaina', nota1: 9, nota2: 10 },
+  { nome: 'Suelen', nota1: 6, nota2: 8 }
 ];
 
-// Função para calcular a média e encontrar o aluno com a maior média
+// Função que calcula a média de cada aluno e retorna o nome do aluno com a maior média.
 function encontrarMelhorAluno(alunos) {
-    // Itera sobre cada aluno e calcula a média
-    alunos.forEach(aluno => {
-        aluno.media = (aluno.nota1 + aluno.nota2) / 2;
-    });
+  // Calcula a média para cada aluno.
+  alunos.forEach(aluno => {
+    aluno.media = (aluno.nota1 + aluno.nota2) / 2;
+  });
 
-    // Encontra o aluno com a maior média usando reduce
-    const melhorAluno = alunos.reduce((melhor, atual) => {
-        return atual.media > melhor.media ? atual : melhor;
-    });
+  // Encontra o aluno com a maior média.
+  const melhorAluno = alunos.reduce((melhor, atual) => {
+    return atual.media > melhor.media ? atual : melhor;
+  });
 
-    return melhorAluno.nome;
+  // Retorna o nome do aluno com a maior média.
+  return melhorAluno.nome;
 }
 
-// Chama a função e exibe o resultado em um elemento HTML
+// Encontra o nome do aluno com a maior média.
 const melhorAluno = encontrarMelhorAluno(alunos);
-const resultadoElement = document.getElementById('resultado');
-resultadoElement.textContent = `O aluno com a maior média é: ${melhorAluno}`;
+
+// Exibe o nome do aluno com a maior média no elemento com o ID "resultado".
+document.getElementById('resultado').textContent = `O aluno com a maior média é: ${melhorAluno}`;
